85b01d3cdccc4fe85dafb20ca7fb89d62433354d
YanqingWu <callmeqingqing@gmail.com>
2020-05-06 20:25:06 +0800

    fix model bug

--- models/vanilla_ae.py
+++ models/vanilla_ae.py
@@ -33,29 +33,41 @@ class UpConvBlock(nn.Module):
 
 
 class AutoEncoder(nn.Module):
-    def __init__(self, channels=(3, 32, 64, 128, 256)):
+    def __init__(self, img_channels=3):
         super(AutoEncoder, self).__init__()
-        encoder = []
-        for c in range(len(channels)-1):
-            encoder.append(BasicConvBlock(channels[c], channels[c+1]))
-        self.encoder = nn.Sequential(*encoder)
 
-        decoder = []
-        for c in range(len(channels)-1, 0, -1):
-            decoder.append(BasicConvBlock(channels[c], channels[c-1]))
-        self.decoder = nn.Sequential(*decoder)
+        self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Conv1 = BasicConvBlock(ch_in=img_channels, ch_out=64)
+        self.Conv2 = BasicConvBlock(ch_in=64, ch_out=128)
+        self.Conv3 = BasicConvBlock(ch_in=128, ch_out=256)
+        self.Conv4 = BasicConvBlock(ch_in=256, ch_out=512)
+        self.Conv5 = BasicConvBlock(ch_in=512, ch_out=1024)
+
+        self.Up5 = UpConvBlock(ch_in=1024, ch_out=512)
+        self.Up4 = UpConvBlock(ch_in=512, ch_out=256)
+        self.Up3 = UpConvBlock(ch_in=256, ch_out=128)
+        self.Up2 = UpConvBlock(ch_in=128, ch_out=64)
+        self.Up1 = nn.Conv2d(64, img_channels, kernel_size=1, stride=1, padding=0)
+
+        self.encoder = nn.Sequential(*[self.Conv1, self.Maxpool, self.Conv2, self.Maxpool,
+                                       self.Conv3, self.Maxpool, self.Conv4, self.Maxpool, self.Conv5])
+        self.decoder = nn.Sequential(*[self.Up5, self.Up4, self.Up3, self.Up2, self.Up1])
         init_weights(self.encoder)
         init_weights(self.decoder)
 
     def forward(self, x):
+        # encoding path
         x = self.encoder(x)
+
+        # decoding path
         x = self.decoder(x)
         return x
 
 
 if __name__ == '__main__':
-    x = torch.rand(1, 3, 500, 500)
+    """ img size must be 2^N """
+    x = torch.rand(1, 3, 512, 512)
     model = AutoEncoder()
     y = model(x)
-    assert x.shape == y.shape
+    print(x.shape, y.shape)
 
