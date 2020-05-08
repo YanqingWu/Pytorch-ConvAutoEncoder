84c5f22acb29c2bccf7fb1d6261f2b2efe29060d
YanqingWu <callmeqingqing@gmail.com>
2020-05-08 11:51:22 +0800

    fix augment  bugs

--- utils/augment.py
+++ utils/augment.py
@@ -3,35 +3,45 @@ from albumentations import *
 
 def augment(img):
     aug = Compose([
-        OneOf([RandomGamma(gamma_limit=(30, 120), p=0.5),
-               RandomBrightnessContrast(brightness_limit=0.5, contrast_limit=0.5, p=1)],
-              p=1),
-        OneOf([GaussNoise(var_limit=(10.0, 50.0), mean=0, p=0.5),
-               ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.5), p=0.5),
-               MultiplicativeNoise(multiplier=(0.9, 1.1), per_channel=False, elementwise=False, p=0.5)],
+        # """ flip or transpose """
+        OneOf([HorizontalFlip(p=1),
+               VerticalFlip(p=1),
+               Transpose(p=1)],
               p=0.5),
-        Flip(p=0.5),
-        Transpose(p=0.5),
-        ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.5, rotate_limit=45, p=0.5),
-        OneOf([MotionBlur(p=0.5),
-               MedianBlur(blur_limit=3, p=0.5),
-               Blur(blur_limit=3, p=0.5),
-               GaussianBlur(blur_limit=3, p=0.5)],
-              p=0.5),
-        OneOf([OpticalDistortion(distort_limit=0.05, shift_limit=0.05, p=1),
-               GridDistortion(num_steps=5, distort_limit=0.3, p=1),
-               ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.5)],
+
+        # """ bright or contrast """
+        OneOf([RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0., p=1),
+               RandomBrightnessContrast(brightness_limit=0., contrast_limit=0.3, p=1)
+               ],
               p=1),
-        # color jitter
-        OneOf([HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.5),
-               CLAHE(clip_limit=4.0, tile_grid_size=(8, 8), p=0.5),
-               IAASharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), p=0.5),
-               IAAEmboss(alpha=(0.2, 0.5), strength=(0.2, 0.7), p=0.5),
-               RGBShift(r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, p=0.5),
-               ToGray(p=0.5),
-               ToSepia(p=0.5),
-               InvertImg(p=0.5),
-               IAAPerspective(scale=(0.05, 0.1), keep_size=True, p=0.5)],
+
+        # """ noise """
+        OneOf([GaussNoise(var_limit=(10.0, 50.0), mean=0, p=1),
+               ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.5), p=1),
+               MultiplicativeNoise(multiplier=(0.9, 1.1), per_channel=False, elementwise=False, p=1)],
+              p=1),
+
+
+        # """ scale """
+        ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.5, rotate_limit=1, p=0.5),
+
+        # """ blur """
+        OneOf([MotionBlur(p=1),
+               MedianBlur(blur_limit=3, p=1),
+               Blur(blur_limit=3, p=1),
+               GaussianBlur(blur_limit=3, p=1)],
+              p=0.5),
+
+        # """ shape distortion """
+        OneOf([OpticalDistortion(distort_limit=0.03, shift_limit=0.05, p=1),
+               GridDistortion(num_steps=1, distort_limit=0.1, p=1)],
+              p=1),
+
+        # """ color jitter """
+        OneOf([HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=1),
+               RGBShift(r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, p=1),
+               ToGray(p=1),
+               ToSepia(p=1)],
               p=0.5),
     ], p=1)
     return aug(**{'image': img})['image']
